53781d2fcdc3a0394471c60f5c19d8c243bf4f7a
Melissa Nguyen <nguyen.melissa23@gmail.com>
2020-07-04 13:56:34 -0700

    add redux-persist with local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 // we need Middleware so that when actions get fired and dispatched, we can catch them. 
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const middleware = [logger];  // this is an array.
 
-const store = createStore(rootReducer, applyMiddleware(...middleware)); 
+export const store = createStore(rootReducer, applyMiddleware(...middleware)); 
 //can add more to this array of middleware logger.
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
 
